e4ed9d206d3c32c4e2d4a6899e001332798c1d87
Joao-Quaresma <joao.quaresma1991@gmail.com>
2019-06-07 16:27:33 +0100

    final commit

--- app.js
+++ app.js
@@ -4,12 +4,31 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require('lodash');
+const mongoose = require("mongoose");
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+mongoose.connect("mongodb://localhost:27017/post", {useNewUrlParser: true});
+
+const postsSchema = new mongoose.Schema ({
+  title: {
+    type: String,
+    required: [true, "Please check your data entry, no title specified!"]
+  },
+  content: {
+    type: String,
+    required: [true, "Please check your data entry, no content specified!"]
+  }
+});
+
+const Post = mongoose.model("Post", postsSchema);
+
+const homeStartingContent = new Post({
+  title: "Home",
+  content: "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing."
+});
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-let posts = [];
+defaultPosts = [homeStartingContent];
 
 const app = express();
 
@@ -22,11 +41,22 @@ app.listen(4000, function() {
   console.log("Server started on port 4000");
 });
 
-
 app.get("/", function(req, res){
-  res.render('home', {
-    homeStartingContent: homeStartingContent,
-    posts: posts
+  Post.find({}, function(err, posts){
+    if (posts.length === 0){
+      Post.insertMany(defaultPosts, function(err){
+        if (err){
+          console.log(err);
+        } else {
+          console.log("Default Posts added");
+        }
+      });
+    }
+    if (err){
+      console.log(err);
+    } else {
+      res.render('home', {posts: posts});
+    }
   });
 });
 
@@ -43,25 +73,24 @@ app.get("/compose", function(req, res){
 });
 
 app.post("/compose", function(req, res) {
-  const post = {
-    title: req.body.postTitle,
-    content: req.body.postBody
-  };
+  const title = req.body.postTitle;
+  const content = req.body.postBody;
 
-  posts.push(post);
-
+  const newPost = new Post({
+    title: title,
+    content: content
+  });
+  newPost.save();
+  console.log("added");
   res.redirect("/");
 });
 
-app.get('/posts/:postName', function(req, res) {
-  const postName = req.params.postName;
-
-  let post = posts.find( post => _.lowerCase(post['title']) === _.lowerCase(postName) );
-
-  if (post === undefined || post === null) {
-    console.log("No post found")
-  } else {
-    res.render('post', {postName: post.title, postBody: post.content} );
-  }
-  // res.send(req.params.postName)
+app.get('/posts/:id', function(req, res) {
+  Post.findOne({_id: req.params.id}, function(err, post){
+      if (err){
+        console.log(err);
+      } else {
+        res.render('post', {postName: post.title, postBody: post.content} );
+      }
+  });
 });
